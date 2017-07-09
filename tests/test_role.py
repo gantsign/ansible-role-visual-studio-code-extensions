1e873b9ec26e288ca1dd1e0802116d11a7a7fbec
NileshGule <vn_nilesh@yahoo.com>
2017-07-09 21:30:14 +0800

    corrected whitespace errors

--- tests/test_role.py
+++ tests/test_role.py
@@ -17,4 +17,3 @@ def test_visual_studio_code(Command, extension):
 def test_visual_studio_code_extensions(Command):
     output = Command.check_output('code --list-extensions')
     assert 'donjayamanne.python' in output
-    
