2be0a357219136c1f1181445790489f317eed232
John Freeman <john.freeman.opensource@gmail.com>
2017-07-22 16:53:49 +0100

    Enabled uninstall extension feature on Linux (#25)
    Was mainly disabled to due difficulty getting the tests to pass.

--- tests/test_role.py
+++ tests/test_role.py
@@ -21,11 +21,7 @@ def test_visual_studio_code_extensions(Command):
     assert 'donjayamanne.python' in output
 
 
-# @pytest.mark.parametrize('extension', [
-#     'donjayamanne.python',
-#     'wholroyd.jinja'
-# ])
-# def test_visual_studio_code_uninstall_extensions(Command, extension):
-#     output = Command.check_output('sudo --user test_usr -H code %s %s',
-#                                   '--uninstall-extension', extension)
-#     assert 'successfully uninstalled' in output
+def test_visual_studio_code_uninstall_extensions(Command):
+    output = Command.check_output('sudo --user test_usr -H code %s',
+                                  '--list-extensions')
+    assert 'seanmcbreen.Spell' not in output
