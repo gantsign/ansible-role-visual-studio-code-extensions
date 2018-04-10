a96e46708c632436fc03925176d8f1144ad810fa
John Freeman <john.freeman.opensource@gmail.com>
2018-04-10 08:50:52 +0000

    Fixed Python extension ID in test
    It's now an official Microsoft project.

--- tests/test_role.py
+++ tests/test_role.py
@@ -6,7 +6,7 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 @pytest.mark.parametrize('extension', [
-    'donjayamanne.python',
+    'ms-python.python',
     'wholroyd.jinja'
 ])
 def test_visual_studio_code(Command, extension):
@@ -18,7 +18,7 @@ def test_visual_studio_code(Command, extension):
 def test_visual_studio_code_extensions(Command):
     output = Command.check_output('sudo --user test_usr -H code %s',
                                   '--list-extensions')
-    assert 'donjayamanne.python' in output
+    assert 'ms-python.python' in output
 
 
 def test_visual_studio_code_uninstall_extensions(Command):
