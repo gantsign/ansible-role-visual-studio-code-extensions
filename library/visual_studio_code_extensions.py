db0b19d2420a0ddd26a0abc652b25bc18dbc8a09
John Freeman <john.freeman.opensource@gmail.com>
2018-10-31 16:02:07 +0000

    Added support for upgrading extensions
    If an extension is already installed it will now be upgraded.

---
+++ library/visual_studio_code_extensions.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+# Make coding more python3-ish
+from __future__ import absolute_import, division, print_function
+
+import os
+
+from ansible.module_utils.basic import AnsibleModule
+
+__metaclass__ = type
+
+
+def is_extension_installed(module, name):
+    rc, out, err = module.run_command(['code', '--list-extensions', name])
+    if rc != 0 or err:
+        module.fail_json(
+            msg='Error querying installed extensions [%s]: %s' % (name,
+                                                                  out + err))
+    lowername = name.lower()
+    match = next((x for x in out.splitlines() if x.lower() == lowername), None)
+    return match is not None
+
+
+def list_extension_dirs(module):
+    ext_dir = os.path.expanduser(
+        os.path.join('~', '.vscode', 'extensions'))
+
+    ext_dirs = [f for f in os.listdir(
+        ext_dir) if os.path.isdir(os.path.join(ext_dir, f))]
+    ext_dirs.sort()
+    return ext_dirs
+
+
+def install_extension(module, name):
+    if is_extension_installed(module, name):
+        # Use the fact that extension directories names contain the version number
+        before_ext_dirs = list_extension_dirs(module)
+        # Unfortunately `--force` suppresses errors (such as extension not found)
+        rc, out, err = module.run_command(
+            ['code', '--install-extension', '--force', name])
+        if rc != 0 or err:
+            module.fail_json(
+                msg='Error while upgrading extension [%s]: %s' % (name,
+                                                                  out + err))
+        after_ext_dirs = list_extension_dirs(module)
+        changed = before_ext_dirs != after_ext_dirs
+        return changed, 'upgrade'
+    else:
+        rc, out, err = module.run_command(
+            ['code', '--install-extension', name])
+        if rc != 0 or err:
+            module.fail_json(
+                msg='Error while installing extension [%s]: %s' % (name,
+                                                                   out + err))
+        changed = not 'already installed' in out
+        return changed, 'install'
+
+
+def uninstall_extension(module, name):
+    if is_extension_installed(module, name):
+        rc, out, err = module.run_command(
+            ['code', '--uninstall-extension', name])
+        if rc != 0 or err:
+            module.fail_json(
+                msg='Error while uninstalling extension [%s]: %s' % (name,
+                                                                     out + err))
+        if 'successfully uninstalled' not in out:
+            module.fail_json(
+                msg=('Error while uninstalling extension [%s]:'
+                     ' unexpected response: %s') % (name, out + err))
+        return True
+    else:
+        return False
+
+
+def run_module():
+
+    module_args = dict(
+        name=dict(type='str', required=True),
+        state=dict(type='str', default='present', choices=['absent', 'present']))
+
+    module = AnsibleModule(argument_spec=module_args,
+                           supports_check_mode=False)
+
+    name = module.params['name']
+    state = module.params['state']
+
+    if state == 'absent':
+        changed = uninstall_extension(module, name)
+
+        if changed:
+            msg = '%s is now uninstalled' % name
+        else:
+            msg = '%s is not installed' % name
+    else:
+        changed, change = install_extension(module, name)
+
+        if changed:
+            if change == 'upgrade':
+                msg = '%s was upgraded' % name
+            else:
+                msg = '%s is now installed' % name
+        else:
+            msg = '%s is already installed' % name
+
+    module.exit_json(changed=changed, msg=msg)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
